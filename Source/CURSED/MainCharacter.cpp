fe6b39306368919e3a296a04a1e007851f9d7769
OsamaTnt <30365260+OsamaTnt@users.noreply.github.com>
2017-08-13 04:30:29 +0200

    The Implementation of Character Movement & Camera View & Look Beta001

--- Source/CURSED/MainCharacter.cpp
+++ Source/CURSED/MainCharacter.cpp
@@ -7,8 +7,47 @@
 // Sets default values
 AMainCharacter::AMainCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	//Set The character SkeletalMesh
+	ConstructorHelpers::FObjectFinder<USkeletalMesh>MainCharacter_Mesh(TEXT("SkeletalMesh'/Game/Characters/FemaleWarrior_UE-Scaled_.FemaleWarrior_UE-Scaled_'"));
+	GetMesh()->SetSkeletalMesh(MainCharacter_Mesh.Object);
+	GetMesh()->SetRelativeLocation(FVector(0, 0, -95));
+	GetCapsuleComponent()->SetCapsuleHalfHeight(93);
+
+
+	///Initialize Variables
+	MovementInput.X = MovementInput.Y = 0;
+	MouseAxis.X = MouseAxis.Y = 0;
+
+	//Set the PlayerScence
+	Player_Scene = CreateDefaultSubobject<USceneComponent>(TEXT("PlayerScene"));
+	Player_Scene->AttachTo(GetMesh());
+	Player_Scene->SetRelativeLocation(FVector(0, 0, 150));
+
+	//Set the SpringArmComponent
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArmComponent->AttachTo(Player_Scene);
+	SpringArmComponent->SetRelativeRotation(FRotator(-25, 0, 0));
+	SpringArmComponent->TargetArmLength = 350;
+
+	//Set the Player_CameraComponent
+	Player_CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
+	Player_CameraComponent->AttachTo(SpringArmComponent);
+
+	//Set The Player to be Possess to the World
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+	AutoPossessAI = EAutoPossessAI::Spawned;
+
+	//Set UsingController Rotation to false
+	bUseControllerRotationPitch = bUseControllerRotationRoll = bUseControllerRotationYaw = false;
+
+	//Set Character Movement Properties
+	GetCharacterMovement()->bAlwaysCheckFloor = true;
+	GetCharacterMovement()->bCanWalkOffLedges = true;
+	GetCharacterMovement()->MaxWalkSpeed = 200;
+
 
 }
 
@@ -16,13 +55,21 @@ AMainCharacter::AMainCharacter()
 void AMainCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
-void AMainCharacter::Tick( float DeltaTime )
+void AMainCharacter::Tick(float DeltaTime)
 {
-	Super::Tick( DeltaTime );
+	Super::Tick(DeltaTime);
+
+	Look_UpDown = Player_Scene->GetComponentRotation();
+	Look_UpDown.Pitch += MouseAxis.Y;
+	Player_Scene->SetWorldRotation(Look_UpDown);
+
+	Look_RL = GetActorRotation();
+	Look_RL.Yaw += MouseAxis.X;
+	SetActorRotation(Look_RL);
 
 }
 
@@ -31,5 +78,36 @@ void AMainCharacter::SetupPlayerInputComponent(class UInputComponent* InputCompo
 {
 	Super::SetupPlayerInputComponent(InputComponent);
 
+	///BindAxis to Inputs
+
+	//Bind FB & RL Movement
+	InputComponent->BindAxis("MoveFB", this, &AMainCharacter::MoveFB);
+	InputComponent->BindAxis("MoveRL", this, &AMainCharacter::MoveRL);
+
+	//Bind UpDown & RL Looking
+	InputComponent->BindAxis("LookUpDown", this, &AMainCharacter::LookUpDown);
+	InputComponent->BindAxis("LookRL", this, &AMainCharacter::LookRL);
+
 }
 
+void AMainCharacter::MoveFB(float AxisValue)
+{
+	MovementInput.X = AxisValue;
+	AddMovementInput(GetActorForwardVector(), MovementInput.X);
+}
+
+void AMainCharacter::MoveRL(float AxisValue)
+{
+	MovementInput.Y = AxisValue;
+	AddMovementInput(GetActorRightVector(), MovementInput.Y);
+}
+
+void AMainCharacter::LookUpDown(float AxisValue)
+{
+	MouseAxis.Y = AxisValue;
+}
+
+void AMainCharacter::LookRL(float AxisValue)
+{
+	MouseAxis.X = AxisValue;
+}
--- Source/CURSED/MainCharacter.h
+++ Source/CURSED/MainCharacter.h
@@ -10,22 +10,63 @@ class CURSED_API AMainCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+	///Variables
+
+	//Create a Scene
+	UPROPERTY(EditAnyWhere)
+	USceneComponent *Player_Scene;
+
+	//Create a SpringArm
+	UPROPERTY(EditAnyWhere)
+	USpringArmComponent *SpringArmComponent;
+
+	//Create a CameraComponent
+	UPROPERTY(EditAnyWhere)
+	UCameraComponent *Player_CameraComponent;
+
+	//Vectors & Rotaors
+	UPROPERTY(EditAnyWhere, Category = "Variables_C++")
+	FVector2D MovementInput;
+
+	UPROPERTY(EditAnyWhere, Category = "Variables_C++")
+	//MouseAxis.X (Yaw Aixs) && MouseAxis.Y (Pitch Axis)
+	FVector2D MouseAxis;
+
+	UPROPERTY(EditAnyWhere, Category = "Variables_C++")
+	FRotator Look_RL;
+
+	UPROPERTY(EditAnyWhere, Category = "Variables_C++")
+	FRotator Look_UpDown;
+
+
 public:
 	// Sets default values for this character's properties
 	AMainCharacter();
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
+
 	// Called every frame
-	virtual void Tick( float DeltaSeconds ) override;
+	virtual void Tick(float DeltaSeconds) override;
 
-	
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+	//handle FB Movement
+	UFUNCTION()
+		void MoveFB(float AxisValue);
+
+	//handle RL Movement
+	UFUNCTION()
+		void MoveRL(float AxisValue);
+
+	//handle Camera UpDown Looks 
+	UFUNCTION()
+		void LookUpDown(float AxisValue);
+
+	//handle Camera RL Looks
+	UFUNCTION()
+		void LookRL(float AxisValue);
 
 
-	
-
-		 
-
-	
 };
